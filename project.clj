c5426547abc112ef2b68f3e88830928fe7ded05c
Jonathan Boston <bostonou@gmail.com>
2014-02-06 12:38:50 -0600

    Project description

--- project.clj
+++ project.clj
@@ -1,13 +1,12 @@
 (defproject om-react "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
+  :description "Exploring React/Om by rewriting 'Thinking in React' example (http://facebook.github.io/react/blog/2013/11/05/thinking-in-react.html)"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2138"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [om "0.2.3"]
                  [com.facebook/react "0.8.0.1"]]
-
+  
   :plugins [[lein-cljsbuild "1.0.1"]]
 
   :source-paths ["src"]
